5ed0baf81bee1023dea5d23923692a07c9e69d4f
kumarsiddharth74 <65486296+kumarsiddharth74@users.noreply.github.com>
2020-07-17 22:10:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('student_score.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    #output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Your Exam Score Based On Your Study Hours Is {}%'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
